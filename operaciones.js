4eee08f48a2be5e2cc8a58c371fe820f1cbb3ec6
iesramonllulljessicastoica <al.jessicastoica@iesramonllull.net>
2025-10-03 09:41:22 +0200

    subida javascript 2

---
+++ operaciones.js
@@ -0,0 +1,6 @@
+function restar() {
+    let valor1 = parseInt(document.getElementById('numero 1').value);
+    let valor2 = parseInt(document.getElementById('numero 2').value);
+    let resultado = valor1 - valor2;
+    alert("El resultado es " + resultado);
+  }
